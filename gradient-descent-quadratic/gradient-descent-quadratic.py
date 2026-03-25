688a5415c1e0f7da72cf138cff36c1d8e0dff6e1
AnandvPrajapati <159772603+AnandvPrajapati@users.noreply.github.com>
2026-03-25 16:11:51 +0530

    Add solution for gradient-descent-quadratic

---
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -0,0 +1,8 @@
+def gradient_descent_quadratic(a, b, c, x0, lr, steps):
+    x = float(x0)
+
+    for i in range(steps):
+        grad = 2 * a * x + b
+        x = x - lr * grad
+
+    return float(x)
